288c11605c1234c181bb77fed36dcd8834728433
Otavio <correaotavio0409@gmail.com>
2024-09-09 11:29:08 -0300

    Passagem de parametro por copia

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <limits>
+#include <iostream>
+
+bool readInt(uint8_t value)
+{
+    std::cin >> value;
+    if(std::cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<uint8_t>::max(), 'n');
+        return false;
+    }
+    return true;
+}
+
+
+// void test(int a)
+// {
+//     a = 10;
+//     std::cout << "a: " << a << '\n';
+// }
+
+int main()
+{
+
+    uint16_t v = 0;
+    std::cout << "Type number: ";
+    if(!readInt(v)){
+        std::cout << "Number invalid \n";
+    }else{
+        std::cout << "The number is: " << v << '\n';
+    }
+
+
+
+    //passagem de parametro por cópia
+    // int x = 100;
+    // std::cout << "Antes x: " << x << '\n';
+    // test(x);
+    // std::cout << "Depois x: " << x << '\n';
+    return 0;
+
+}
